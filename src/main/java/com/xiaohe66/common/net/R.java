b03ba4b5348cfe549e94a9f46552ee9a1e8dec04
xiaohe <ty@xiaohe66.com>
2021-09-24 14:01:37 +0800

    c : R类

---
+++ src/main/java/com/xiaohe66/common/net/R.java
@@ -0,0 +1,55 @@
+package com.xiaohe66.common.net;
+
+import lombok.Getter;
+
+/**
+ * @author xiaohe
+ * @since 2021.08.13 14:46
+ */
+@Getter
+public class R<T> {
+
+    public static final int SUCCESS_CODE = 10000;
+    public static final int ERROR_CODE = -1;
+
+    public static final R<Void> SUCCESS = R.ok();
+    public static final R<Void> FAIL = R.err("");
+
+    private int code;
+    private String msg;
+    private T data;
+
+    private R(int code, T data, String msg) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public static R<Void> ok() {
+        return SUCCESS;
+    }
+
+    public static <T> R<T> ok(T data) {
+        return new R<>(SUCCESS_CODE, data, null);
+    }
+
+    public static <T> R<T> ok(T data, String msg) {
+        return new R<>(SUCCESS_CODE, data, msg);
+    }
+
+    public static R<Void> err() {
+        return FAIL;
+    }
+
+    public static R<Void> err(String msg) {
+        return new R<>(ERROR_CODE, null, msg);
+    }
+
+    public static R<Void> build(int code, String msg) {
+        return new R<>(code, null, msg);
+    }
+
+    public static <T> R<T> build(int code, String msg, T data) {
+        return new R<>(code, data, msg);
+    }
+}
