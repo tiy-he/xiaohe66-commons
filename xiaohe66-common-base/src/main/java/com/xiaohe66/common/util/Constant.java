031929e7dfc73739362613f2432e058f3c732c4c
xiaohe <ty@xiaohe66.com>
2021-11-16 17:12:02 +0800

    feat(增加常量池):

---
+++ xiaohe66-common-base/src/main/java/com/xiaohe66/common/util/Constant.java
@@ -0,0 +1,72 @@
+package com.xiaohe66.common.util;
+
+/**
+ * @author xiaohe
+ * @since 2021.11.03 10:08
+ */
+public class Constant {
+
+    protected Constant() {
+    }
+
+    public static class Str {
+
+        protected Str() {
+        }
+
+        public static final String AMPERSAND = "&";
+        public static final String AND = "and";
+        public static final String AT = "@";
+        public static final String ASTERISK = "*";
+        public static final String STAR = ASTERISK;
+        public static final String BACK_SLASH = "\\";
+        public static final String COLON = ":";
+        public static final String COMMA = ",";
+        public static final String DASH = "-";
+        public static final String DOLLAR = "$";
+        public static final String DOT = ".";
+        public static final String DOTDOT = "..";
+        public static final String EMPTY = "";
+        public static final String EQUALS = "=";
+        public static final String FALSE = "false";
+        public static final String SLASH = "/";
+        public static final String HASH = "#";
+        public static final String HAT = "^";
+        public static final String LEFT_BRACE = "{";
+        public static final String LEFT_BRACKET = "(";
+        public static final String LEFT_CHEV = "<";
+        public static final String NEWLINE = "\n";
+        public static final String N = "n";
+        public static final String NO = "no";
+        public static final String NULL = "null";
+        public static final String OFF = "off";
+        public static final String ON = "on";
+        public static final String PERCENT = "%";
+        public static final String PIPE = "|";
+        public static final String PLUS = "+";
+        public static final String QUESTION_MARK = "?";
+        public static final String EXCLAMATION_MARK = "!";
+        public static final String QUOTE = "\"";
+        public static final String RETURN = "\r";
+        public static final String TAB = "\t";
+        public static final String RIGHT_BRACE = "}";
+        public static final String RIGHT_BRACKET = ")";
+        public static final String RIGHT_CHEV = ">";
+        public static final String SEMICOLON = ";";
+        public static final String SINGLE_QUOTE = "'";
+        public static final String BACKTICK = "`";
+        public static final String SPACE = " ";
+        public static final String TILDA = "~";
+        public static final String LEFT_SQ_BRACKET = "[";
+        public static final String RIGHT_SQ_BRACKET = "]";
+        public static final String TRUE = "true";
+        public static final String UNDERSCORE = "_";
+        public static final String Y = "y";
+        public static final String YES = "yes";
+        public static final String ONE = "1";
+        public static final String ZERO = "0";
+        public static final String CRLF = "\r\n";
+
+    }
+
+}
