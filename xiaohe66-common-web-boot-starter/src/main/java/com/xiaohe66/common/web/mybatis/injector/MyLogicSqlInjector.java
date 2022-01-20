caa904e6faa572c7a765994d409032ba1ef71ca0
xiaohe <ty@xiaohe66.com>
2022-01-20 17:17:08 +0800

    升级mybatis-plus到3.5.0

--- xiaohe66-common-web-boot-starter/src/main/java/com/xiaohe66/common/web/mybatis/injector/MyLogicSqlInjector.java
+++ xiaohe66-common-web-boot-starter/src/main/java/com/xiaohe66/common/web/mybatis/injector/MyLogicSqlInjector.java
@@ -2,6 +2,7 @@ package com.xiaohe66.common.web.mybatis.injector;
 
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
 
 import java.util.List;
 
@@ -12,8 +13,8 @@ import java.util.List;
 public class MyLogicSqlInjector extends DefaultSqlInjector {
 
     @Override
-    public List<AbstractMethod> getMethodList(Class<?> mapperClass) {
-        List<AbstractMethod> list = super.getMethodList(mapperClass);
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        List<AbstractMethod> list = super.getMethodList(mapperClass, tableInfo);
 
         list.add(new IsExistId());
         list.add(new IsExist());
